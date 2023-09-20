bf34b49cd53f13d5bd08cf99ee9603b1c761689a
raghava139 <rraghava139@gmail.com>
2023-09-21 01:06:26 +0530

    Radix Sort

--- JsEngine/Closures.js
+++ JsEngine/Closures.js
@@ -7,7 +7,7 @@
 //     console.log('f-a',a);
 //   }
 //   return y
-  
+
 // }
 // var test = closures();
 
@@ -25,20 +25,38 @@
 // }
 // var test = z();
 // console.log(test())
-function Long(){
-    function z(){
-        var a=20;
-        function inside(){
-            var b=100;
+// function Long(){
+//     function z(){
+//         var a=20;
+//         function inside(){
+//             var b=100;
 
-            function insideanother(){       
-                console.log(a,b);
-            }
-            insideanother();
-        }
-        inside();
-    }
-    z();
-    
+//             function insideanother(){
+//                 console.log(a,b);
+//             }
+//             insideanother();
+//         }
+//         inside();
+//     }
+//     z();
+
+// }
+// var test = Long()
+
+//closures with setTimeout
+
+function Testclosure() {
+    var i=0;
+  for (let i = 1; i <= 10; i++) {
+
+      setTimeout(() => {
+        console.log(i);
+      }, 1000);
+  }
 }
-var test = Long()
+Testclosure();
+
+
+(function(){
+    console.log('adfa');
+})()
--- Udemy/Sorting Algorithms/Radix Sort.js
+++ Udemy/Sorting Algorithms/Radix Sort.js
@@ -1,4 +1,43 @@
-function RadixSort(){
-        console.log('radix sort');
+//get Digit Number(using index)
+function getDigit(num, i) {
+//   console.log('getdigit',Math.floor(Math.abs(num)/Math.pow(10,i)) % 10)
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
-RadixSort();
+// getDigit(7323,3);
+
+// get digit count means length
+function digitCount(num) {
+  if (num == 0) return 1;
+//   console.log('digitcount',Math.floor(Math.log10(Math.abs(num))) + 1);
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// digitCount(1222);
+
+//find the max value of an array
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  console.log("maxdigit", maxDigits);
+  return maxDigits;
+}
+// mostDigits([23,567,89,1234567890,90]);
+
+function radixSort(nums) {
+  let maxdigitcount = mostDigits(nums);
+
+  for (let k = 0; k < maxdigitcount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digitValue = getDigit(nums[i], k);
+      digitBuckets[digitValue].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+    console.log("digit buckets", digitBuckets);
+  }
+  console.log("nums", nums);
+  return nums;
+}
+radixSort([23, 345, 567, 12, 2345, 9852]);
